842006643c122b7289b53907a130fad85b888d68
Mumtaz Lone <mumtazlone1315@gmail.com>
2025-12-30 20:23:55 +0530

    Create basic.py

---
+++ basic.py
@@ -0,0 +1,19 @@
+# This program shows basic use of variables and data types
+
+# String data type
+name = "Mumtaz"
+
+# Integer data type
+age = 20
+
+# Float data type
+percentage = 85.5
+
+# Boolean data type
+is_student = True
+
+# Displaying the values
+print(name)
+print(age)
+print(percentage)
+print(is_student)
