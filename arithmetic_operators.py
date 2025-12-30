460bb857242583b62c26f095e9cd6fa573eb675b
Mumtaz Lone <mumtazlone1315@gmail.com>
2025-12-30 20:33:52 +0530

    Create arithmetic_operators.py

---
+++ arithmetic_operators.py
@@ -0,0 +1,21 @@
+# Arithmetic Operators Example
+
+a = 20
+b = 6
+
+sum_result = a + b        # Addition
+diff_result = a - b       # Subtraction
+prod_result = a * b       # Multiplication
+div_result = a / b        # Division
+mod_result = a % b        # Modulus
+floor_div = a // b        # Floor division
+power = a ** 2            # Exponentiation
+
+print("Arithmetic Operators Output")
+print(sum_result)
+print(diff_result)
+print(prod_result)
+print(div_result)
+print(mod_result)
+print(floor_div)
+print(power)
