d21e7a6bdc357dc3abe9ea9e31622587bf4eb773
Mumtaz Lone <mumtazlone1315@gmail.com>
2026-01-08 22:36:27 +0530

    Create implicit_typecasting.py

---
+++ implicit_typecasting.py
@@ -0,0 +1,22 @@
+# Implicit Type Casting Example
+# Python automatically converts one data type into another
+
+a = 10              # Integer value
+b = 5.5             # Float value
+
+result = a + b      # Integer is converted into float automatically
+
+print("Value of a:", a)
+print("Value of b:", b)
+print("Result:", result)
+
+print("Type of a:", type(a))
+print("Type of b:", type(b))
+print("Type of result:", type(result))
+
+x = 3               # Integer
+y = 2.0             # Float
+
+sum_value = x + y
+print("Sum:", sum_value)
+print("Type of sum:", type(sum_value))
