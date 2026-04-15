868a2c4b4732883ec50e5c4b8c96485995c2192c
lonemumtaz <mumtazlone1315@gmail.com>
2026-04-15 23:12:05 +0530

    updating hello.py

--- hello.py
+++ hello.py
@@ -1,1 +1,4 @@
-print("Hello, World!")
+print("Hello, World!") 
+print("Welcome to Python programming!")
+print("This is a simple Python script.")
+print("You can run this script to see the output.")
